4c6d73715572399a24c90e57d355311077d2cb5c
Pedro Almeida <pedro--luiz@outlook.com>
2024-02-20 18:19:06 -0300

    feat: update version

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR. v2
+          Edit <code>src/App.tsx</code> and save to test HMR. v3
         </p>
       </div>
       <p className="read-the-docs">
