f0520f38d4c098dbc4d15f5972c22dc6b4710cc0
Pedro Almeida <pedro--luiz@outlook.com>
2024-02-20 18:55:51 -0300

    chore: bump version

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR. v4
+          Edit <code>src/App.tsx</code> and save to test HMR. v5
         </p>
       </div>
       <p className="read-the-docs">
