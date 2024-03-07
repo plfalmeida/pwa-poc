c090d56e585f290442f6c159e096ac59deb84519
Pedro Almeida <pedro--luiz@outlook.com>
2024-03-07 10:25:47 -0300

    fix: try asset change

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React 5</h1>
+      <h1 className='text-indigo-400'>Vite + React 5</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
